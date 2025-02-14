05e14eae4e603f4233368fee638b460bdce14b14
saijeet <santy.saijeet1996@gmail.com>
2025-02-13 21:50:48 -0800

    day-t3

---
+++ day-t3/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    type = string
+    default = "ami-053a45fff0a704a47"
+  
+}
+
+variable "type" {
+    type = string
+    default = "t2.micro"
+  
+}
+
+variable "key" {
+    type = string
+    default = "saikey"
+  
+}
